73d977761d64f6ae57077ee6d38ec5339d9a4f5e
Ostap <ostap.hnatenko@gmail.com>
2021-08-27 10:02:48 +0300

    Removed Task related stuff from HistoryStore.

--- Shared/Models/HistoryStore.swift
+++ Shared/Models/HistoryStore.swift
@@ -12,37 +12,4 @@ import OrderedCollections
 
 class HistoryStore: ObservableObject {
     @Published var histories = [History]()
-    @Published var policies = OrderedDictionary<UUID, Task>()
-    @Published var policyId = Task.default.id
-    
-    var policyArray: [Task] {
-        Array(policies.values)
-    }
-    
-    var currentPolicy: Task {
-        policy(for: policyId)
-    }
-    
-    func policy(for id: UUID) -> Task {
-        policies[id] ?? .default
-    }
-    
-    func binding(for policy: Task) -> Binding<Task> {
-        .init(
-            get: { self.policy(for: policy.id) },
-            set: { [self] in policies[policy.id] = $0 }
-        )
-    }
-    
-    func insert(policy: Task) {
-        policies[policy.id] = policy
-    }
-    
-    func deletePolicy(at index: Int) {
-        policies.remove(at: index)
-    }
-    
-    init() {
-        insert(policy: .default)
-    }
 }
