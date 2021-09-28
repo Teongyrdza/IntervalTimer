ef052b0a339b29bf1ae216032b60beaad412171f
Ostap <ostap.hnatenko@gmail.com>
2021-09-28 15:41:09 +0300

    Add startup time measurement.

--- Shared/IntervalTimerApp.swift
+++ Shared/IntervalTimerApp.swift
@@ -13,9 +13,16 @@ struct IntervalTimerApp: App {
     @StateObject var historyStore = HistoryStore.load()
     @StateObject var taskStore = TaskStore.load()
     
+    let startTime = Date().timeIntervalSince1970
+    
     var body: some Scene {
         WindowGroup {
             AppView(settings: settings, historyStore: historyStore, taskStore: taskStore)
+                .onAppear {
+                    let now = Date().timeIntervalSince1970
+                    let elapsed = now - startTime
+                    print("Startup took \(elapsed, specifier: "%.2f") seconds")
+                }
         }
     }
 }
